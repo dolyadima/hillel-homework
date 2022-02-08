da30391245433362f5e7071cb6624e9793df3672
dolyadima <dimchik96@gmail.com>
2022-02-08 17:06:09 +0200

    fix leading slash, add host=0.0.0.0

--- flask_app/flask_application.py
+++ flask_app/flask_application.py
@@ -8,35 +8,35 @@ app = Flask(__name__)
 
 @app.route('/')
 def my_main():
-    return '<a href="./whoami">whoami</a><br />' \
-           '<a href="./source_code">source_code</a><br />' \
-           '<a href="./random?length=42&specials=1&digits=0">random</a><br />' \
+    return '<a href="/whoami/">whoami</a><br />' \
+           '<a href="/source_code/">source_code</a><br />' \
+           '<a href="/random/?length=42&specials=1&digits=0">random</a><br />' \
            'Homework 4 Flask Application'
 
 
-@app.route('/whoami')
+@app.route('/whoami/')
 def my_whoami():
     ip_address: str = str(request.remote_addr)
     user_agent: str = str(request.headers.get('User-Agent'))
     server_time: str = str(datetime.now().strftime('%H:%M:%S'))
 
-    return f'<a href="./">&lt;--- back...</a><br />' \
+    return f'<a href="/">&lt;--- back...</a><br />' \
            f'Your ip: {ip_address}<br />' \
            f'Your browser: {user_agent}<br />' \
            f'Server time: {server_time}'
 
 
-@app.route('/source_code')
+@app.route('/source_code/')
 def my_source_code():
     with open(__file__, 'r') as fr:
         lines_file = fr.readlines()
     code_of_script = escape("".join(lines_file))
 
-    return f'<a href="./">&lt;--- back...</a><br />' \
+    return f'<a href="/">&lt;--- back...</a><br />' \
            f'<pre>{code_of_script}</pre>'
 
 
-@app.route('/random')
+@app.route('/random/')
 def my_random():
     try:
         length: int = int(request.values.get('length', 0))
@@ -67,7 +67,7 @@ def my_random():
         for i in range(length):
             result += english_sym[randint(0, len(english_sym)-1)]
 
-    return f'<a href="./">&lt;--- back...</a><br />' \
+    return f'<a href="/">&lt;--- back...</a><br />' \
            f'Generated dataset: [{english_sym}]<br />' \
            f'Length_of_str: {length}<br />' \
            f'Special_characters: {bool(specials)}<br />' \
@@ -75,4 +75,4 @@ def my_random():
            f'Result: {result}'
 
 
-app.run(debug=True)
+app.run(debug=True, host='0.0.0.0')
